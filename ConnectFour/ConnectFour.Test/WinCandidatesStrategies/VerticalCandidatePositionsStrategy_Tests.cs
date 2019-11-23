7c48ffa58c8e12cbdf637db21395c4974005d8fc
Bartosz Cybula <bartosz.cybula@pl.abb.com>
2019-11-23 17:44:52 +0100

    vertical strategy added

---
+++ ConnectFour/ConnectFour.Test/WinCandidatesStrategies/VerticalCandidatePositionsStrategy_Tests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConnectFour.Models;
+using ConnectFour.WinCandidatesStrategies;
+using FluentAssertions;
+using Xunit;
+
+namespace ConnectFour.Test.WinCandidatesStrategies
+{
+    public class VerticalCandidatePositionsStrategy_Tests
+    {
+        public static IEnumerable<object[]> GetPoints()
+        {
+            yield return new object[]
+            {
+                new Point(0, 0),
+                new List<Point>()
+                {
+                    new Point(0,0),
+                    new Point(0,1),
+                    new Point(0,2),
+                    new Point(0,3)
+                }
+            };
+
+            yield return new object[]
+            {
+                new Point(1, 1),
+                new List<Point>()
+                {
+                    new Point(1,1),
+                    new Point(1,2),
+                    new Point(1,3),
+                    new Point(1,4)
+                }
+            };
+
+            yield return new object[]
+            {
+                new Point(2, 2),
+                new List<Point>()
+                {
+                    new Point(2,2),
+                    new Point(2,3),
+                    new Point(2,4),
+                    new Point(2,5)
+                }
+            };
+
+            yield return new object[]
+            {
+                new Point(3, 3),
+                new List<Point>()
+            };
+            yield return new object[]
+            {
+                new Point(4, 4),
+                new List<Point>()
+            };
+            yield return new object[]
+            {
+                new Point(5, 5),
+                new List<Point>()
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetPoints))]
+        public void GetCandidates_WhenCalled_ShouldReturnExpectedValues(Point currentPosition, List<Point> expectedResult)
+        {
+            var sut = new VerticalCandidatePositionsStrategy(6, 6);
+
+            var candidates = sut.GetCandidates(currentPosition);
+            candidates.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
---
+++ ConnectFour/ConnectFour/WinCandidatesStrategies/VerticalCandidatePositionsStrategy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using ConnectFour.Models;
+
+namespace ConnectFour.WinCandidatesStrategies
+{
+    public class VerticalCandidatePositionsStrategy : WiningCandidatePositionsStrategy
+    {
+        public VerticalCandidatePositionsStrategy(int height, int length) : base(height, length)
+        {
+
+        }
+
+        public override IEnumerable<Point> GetCandidates(Point point)
+        {
+            var pointsToCheck = new List<Point>();
+            if (point.Y + WinningRangeLength <= Height)
+            {
+                pointsToCheck.AddRange(Get(point));
+            }
+            return pointsToCheck;
+        }
+
+        private IEnumerable<Point> Get(Point point)
+        {
+            var pointsToCheck = new List<Point>();
+            for (int i = 0; i < WinningRangeLength; i++)
+            {
+                pointsToCheck.Add(new Point(point.X, point.Y + i));
+            }
+            return pointsToCheck;
+        }
+    }
+}
