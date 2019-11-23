d15d3a5e4e7d809e1b419b8e9e7c1e7b04c750ca
Bartosz Cybula <bartosz.cybula@pl.abb.com>
2019-11-23 17:31:03 +0100

    more tests added

--- ConnectFour/ConnectFour.Test/Strategies/HorizontalRightCandidatePositionsStrategy_Tests.cs
+++ ConnectFour/ConnectFour.Test/Strategies/HorizontalRightCandidatePositionsStrategy_Tests.cs
@@ -8,43 +8,31 @@ using Xunit;
 
 namespace ConnectFour.Test.Strategies
 {
-    public class HorizontalLeftCandidatePositionsStrategy_Tests
+    public class HorizontalRightCandidatePositionsStrategy_Tests
     {
         public static IEnumerable<object[]> GetPoints()
         {
             yield return new object[]
             {
-                new Point(5, 1),
+                new Point(0, 1),
                 new List<Point>()
                 {
-                    new Point(5,1),
-                    new Point(4,1),
-                    new Point(3,1),
-                    new Point(2,1)
-                }
-            };
-
-            yield return new object[]
-            {
-                new Point(4, 1),
-                new List<Point>()
-                {
-                    new Point(4,1),
-                    new Point(3,1),
-                    new Point(2,1),
-                    new Point(1,1)
-                }
-            };
-
-            yield return new object[]
-            {
-                new Point(3, 1),
-                new List<Point>()
-                {
-                    new Point(3,1),
-                    new Point(2,1),
+                    new Point(0,1),
                     new Point(1,1),
-                    new Point(0,1)
+                    new Point(2,1),
+                    new Point(3,1)
+                }
+            };
+
+            yield return new object[]
+            {
+                new Point(1, 1),
+                new List<Point>()
+                {
+                    new Point(1,1),
+                    new Point(2,1),
+                    new Point(3,1),
+                    new Point(4,1)
                 }
             };
 
@@ -52,17 +40,27 @@ namespace ConnectFour.Test.Strategies
             {
                 new Point(2, 1),
                 new List<Point>()
+                {
+                    new Point(2,1),
+                    new Point(3,1),
+                    new Point(4,1),
+                    new Point(5,1)
+                }
             };
 
             yield return new object[]
             {
-                new Point(1, 1),
+                new Point(3, 1),
                 new List<Point>()
             };
-
             yield return new object[]
             {
-                new Point(0, 1),
+                new Point(4, 1),
+                new List<Point>()
+            };
+            yield return new object[]
+            {
+                new Point(5, 1),
                 new List<Point>()
             };
         }
@@ -71,7 +69,7 @@ namespace ConnectFour.Test.Strategies
         [MemberData(nameof(GetPoints))]
         public void GetCandidates_WhenCalled_ShouldReturnExpectedValues(Point currentPosition, List<Point> expectedResult)
         {
-            var sut = new HorizontalLeftCandidatePositionsStrategy(6, 6);
+            var sut = new HorizontalRightCandidatePositionsStrategy(6, 6);
 
             var candidates = sut.GetCandidates(currentPosition);
             candidates.Should().BeEquivalentTo(expectedResult);
---
+++ ConnectFour/ConnectFour/Strategy/HorizontalRightCandidatePositionsStrategy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConnectFour.Models;
+
+namespace ConnectFour.Strategy
+{
+    public class HorizontalRightCandidatePositionsStrategy : WiningCandidatePositionsStrategy
+    {
+        public HorizontalRightCandidatePositionsStrategy(int height, int length) : base(height, length)
+        {
+
+        }
+
+        public override IEnumerable<Point> GetCandidates(Point point)
+        {
+            var pointsToCheck = new List<Point>();
+            if (point.X + WinningRangeLength - 1 < Length)
+            {
+                pointsToCheck.AddRange(Get(point));
+            }
+            return pointsToCheck;
+        }
+
+        private IEnumerable<Point> Get(Point point)
+        {
+            var pointsToCheck = new List<Point>();
+            for (int i = 0; i < WinningRangeLength; i++)
+            {
+                pointsToCheck.Add(new Point(point.X + i, point.Y));
+            }
+            return pointsToCheck;
+        }
+    }
+}
